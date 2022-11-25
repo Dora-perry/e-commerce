f0d6e6a9a25ce56e960554b480a3fed1f0ec13fc
Decagon <theodorachinonso@gmail.com>
2022-11-25 18:00:27 +0100

    frontend and backend setup

---
+++ backend/data.js
@@ -0,0 +1,56 @@
+const data = {
+    products: [
+        {
+            name: 'Nike slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'shirts',
+            images: '/images/p1.jpeg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt'
+        },
+        {
+            name: 'Nike slim pants',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            images: '/images/p3.jpeg',
+            price: 50,
+            countInStock: 15,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality pants'
+        },
+        {
+            name: 'Adidas Fit Pants',
+            slug: 'adidas-fit-pants',
+            category: 'Pants',
+            images: '/images/p4.jpeg',
+            price: 80,
+            countInStock: 5,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            name: 'Adidas Fit Polo',
+            slug: 'adidas-fit-polo',
+            category: 'Shirt',
+            images: '/images/p4.jpeg',
+            price: 100,
+            countInStock: 10,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+    ],
+
+    
+}
+
+export default data
